a01c8c1a1445054f4217314c4f6108f8a7f34e26
Juan Pablo <juanpablogast@gmail.com>
2021-11-25 16:54:56 -0300

    SCOPE products

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,12 +9,14 @@ class Product < ApplicationRecord
   validates :code, presence: {message: "Es necesario definir un valor para el codigo"}
   validates :code, uniqueness: {message: "El codigo: %{value} ya se encuentra en uso"}
   validates :price, length: { in: 3..10, message: "El precio se encuentra fera de rango (Min: 3, Max:10)" }, if: :has_price? 
-
-
   validates_with ProductValidator
-
   validate :code_validate
     
+  scope :available, -> (min=1) { where("stock >= ?", min) }
+  scope :order_price_desc, -> { order("price DESC") }
+  scope :available_and_order_price_desc, ->  { available.order_price_desc }
+
+
   def has_price?
     !self.price.nil? && self.price > 0
   end
