ef6b7b16a91ff0a959a27f35626f653d893f5ea4
Juan Pablo <juanpablogast@gmail.com>
2021-11-25 10:49:23 -0300

    validaciones propias

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,9 +8,9 @@ class Product < ApplicationRecord
   validates :title, presence: {message: "Es necesario definir un valor para el titulo"}
   validates :code, presence: {message: "Es necesario definir un valor para el codigo"}
   validates :code, uniqueness: {message: "El codigo: %{value} ya se encuentra en uso"}
-
-  # validates :price, length: { minimum: 3, maximum: 10 }
   validates :price, length: { in: 3..10, message: "El precio se encuentra fera de rango (Min: 3, Max:10)" }, if: :has_price? 
+
+  validate :code_validate
     
   def has_price?
     !self.price.nil? && self.price > 0
@@ -25,6 +25,13 @@ class Product < ApplicationRecord
   end
 
   private 
+
+  def code_validate
+    if self.code.nil? || self.code.length < 3
+      self.errors.add(:code, "El codigo debe posear por lo menos 3 caracteres")
+    end
+  end
+
   def push_notificacion
     puts "\n\n\n >>>> Un nuevo producto en descuento ya se encuentra disponible #{self.title}!"
   end
